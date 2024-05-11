b958618c2cc5cea00add1bf5cdb4f2db9a2f0702
Yasharth Yadav <Yashyadav9695@gmail.com>
2024-05-11 16:08:09 +0530

    modal overlay built 2.0

--- src/components/genai/genai.jsx
+++ src/components/genai/genai.jsx
@@ -3,7 +3,7 @@ function genai(){
 return(
     <>
        <div className="logo_design">
-            <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg" alt="pic" />
+            <img src="../../assets/logo.png" alt="pic" />
             <p className='para'>GenAI Stack</p>
         </div>
         <hr />
